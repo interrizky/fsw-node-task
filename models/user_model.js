52924b031e2fb961d73e776c362b3711b5e1cbe9
Rizky Ridho <interrizky@ymail.com>
2021-10-24 11:47:20 +0700

    add models

---
+++ models/user_model.js
@@ -0,0 +1,26 @@
+/* Create Schema */
+const Mongoose = require('mongoose');
+
+var schemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  },
+  toObject: {
+    virtuals: true
+  }
+};
+
+/*
+var userSchema = new Mongoose.Schema({
+  username: { type: String },
+  password: { type: String },  
+  email: { type: String },
+  address: { type: String },
+  age: { type: String }
+}, schemaOptions)
+*/
+
+// user_registration adalah nama schema / tabel di MangoDB-nya
+const user = Mongoose.model('user_registration', userSchema);
+module.exports = user;
